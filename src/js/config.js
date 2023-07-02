7e75b9b19fb100051df870bf5a9b97632e62614c
tornikegotva <tornikegotvadze77@gmail.com>
2023-07-02 16:42:30 +0400

    Innitial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '49c36a07-a479-4d3c-a078-45ee5f425e55';
+export const MODAL_CLOSE_SEC = 2.5;
